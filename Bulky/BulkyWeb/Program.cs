ee503c5b2e05c46375e60174c8589151d01e8978
Akash Jatwa <akashjatwa85@gmail.com>
2024-10-22 01:55:56 +0530

    added category table

--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -1,7 +1,16 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configuring the DB with connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
